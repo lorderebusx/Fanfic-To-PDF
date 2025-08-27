433563e79fe0ac0b51325f07cdcd59a9d1cf307c
Shubh Gupta <shubhgupta1510@gmail.com>
2025-08-27 16:04:06 -0500

    variables

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
-from scrapers.scrape_fanfiction import scrape_fanfiction
+from scrapers.scrape_fanfiction import scrapeFanfiction
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])    
 def index():
     if request.method == 'POST':
         # Get data from the form
@@ -18,7 +18,7 @@ def index():
         # --- This is the core logic ---
         if siteChoice == 'fanfiction':
             print("Starting FanFiction.net scraper...")
-            scrape_fanfiction(storyUrl, dirName) # You would call your actual function here
+            scrapeFanfiction(storyUrl, dirName) # You would call your actual function here
         elif siteChoice == 'ao3':
             print("Starting ArchiveOfOurOwn.org scraper...")
             # scrape_ao3(story_url) # You would call the AO3-specific function here
