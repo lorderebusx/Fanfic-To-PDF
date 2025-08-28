6c4d7ea32fbf6d96eff316e7a82e77cb24e18b4e
Shubh Gupta <shubhgupta1510@gmail.com>
2025-08-27 23:33:35 -0500

    updated for ao3

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from scrapers.scrape_fanfiction import scrapeFanfiction
 from scrapers.scrape_general import scrapeGeneral
+from scrapers.scrape_ao3 import scrapeAO3
 
 app = Flask(__name__)
 
@@ -22,7 +23,7 @@ def index():
             scrapeFanfiction(storyUrl, dirName) # You would call your actual function here
         elif siteChoice == 'ao3':
             print("Starting ArchiveOfOurOwn.org scraper...")
-            # scrape_ao3(story_url) # You would call the AO3-specific function here
+            scrapeAO3(storyUrl, dirName)
         elif siteChoice == 'general':
             print("Starting general scraper...")
             scrapeGeneral(storyUrl, dirName)
